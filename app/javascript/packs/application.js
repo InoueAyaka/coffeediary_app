06c6e008f3b05b9db143920e2ad3b8b529fc0862
ayaka <1818066@mwu.jp>
2022-01-14 04:32:53 +0900

    ローディング画面の追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,28 @@ Rails.start();
 ActiveStorage.start();
 
 require("jquery");
+
+$(function(){
+    let $window = $(window),
+        $load = $('#load_animation'),
+        $load_in = $load.find('img');
+
+    //ロゴとLoadingGIFをフェードイン
+    $load_in.delay(500).fadeIn('slow');
+
+    //ウィンドウに対してloadイベントを設定
+    //全ての要素をロードが終わったら，HideLoadingScreen()を実行する
+    $window.on('load',function(){
+        HideLoadingScreen();
+    })
+    
+    //５秒後に強制的にHideLoadingScreen()を実行
+    setTimeout(function(){
+        HideLoadingScreen();
+    },5000);
+
+    //ロード画面を非表示にする関数
+    function HideLoadingScreen(){
+        $load.delay(1500).fadeOut("slow");
+    }
+}); 
